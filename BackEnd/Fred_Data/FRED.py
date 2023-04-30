380a82384182bdbb5935788bf453bfcd16b54486
Trey Davidson <treydavidson05@icloud.com>
2023-04-29 22:02:05 -0500

    10 PRE-Start

--- BackEnd/Fred_Data/FRED.py
+++ BackEnd/Fred_Data/FRED.py
@@ -1,25 +1,48 @@
 import requests
 import pandas as pd
+import PASSWORD
 
 # Set up API endpoint and parameters
-url = 'https://api.stlouisfed.org/fred/series/observations'
-params = {
-    'series_id': 'GDP', # The series ID for US GDP
-    'api_key': 'c77fb902ec44501e7dc34fb01c0bc435', # Replace YOUR_API_KEY with your FRED API key
-    'file_type': 'json',
-    'frequency': 'q' # Set frequency to quarterly
-}
 
-# Make the API request
-response = requests.get(url, params=params)
 
-# Convert JSON response to a DataFrame
-data = pd.json_normalize(response.json()['observations'])
-df = pd.DataFrame(data, columns=['date', 'value'])
 
-# Save the DataFrame to a CSV file in a specific directory
-output_path = '/Users/treydavidson/PycharmProjects/YFINANCE/CSV_Data/GDP.csv'
-df.to_csv(output_path, index=False)
+def update_GDP_Data():
+    url = 'https://api.stlouisfed.org/fred/series/observations'
+    params = {
+        'series_id': 'GDP', # The series ID for US GDP
+        'api_key': PASSWORD.API_KEY, # Replace YOUR_API_KEY with your FRED API key
+        'file_type': 'json',
+        'frequency': 'q' # Set frequency to quarterly
+    }
 
-# Print the DataFrame
-print(df)
+    # Make the API request
+    response = requests.get(url, params=params)
+
+    # Convert JSON response to a DataFrame
+    data = pd.json_normalize(response.json()['observations'])
+    df = pd.DataFrame(data, columns=['date', 'value'])
+
+    # Save the DataFrame to a CSV file in a specific directory
+    output_path = 'GDP.csv'
+    df.to_csv(output_path, index=False)
+
+def update_Recession_Data():
+    # Set up API endpoint and parameters
+    url = 'https://api.stlouisfed.org/fred/series/observations'
+    params = {
+        'series_id': 'USREC',  # The series ID for US recession data
+        'api_key': PASSWORD.API_KEY,  # Replace YOUR_API_KEY with your FRED API key
+        'file_type': 'json'
+    }
+
+    # Make the API request
+    response = requests.get(url, params=params)
+
+    # Convert the API response to a pandas DataFrame
+    data = response.json()['observations']
+    df = pd.DataFrame.from_records(data, columns=['realtime_start', 'realtime_end', 'date', 'value'])
+
+    # Save the DataFrame to a CSV file
+    df.to_csv('Recessions.csv', index=False)
+
+update_Recession_Data()
--- BackEnd/Fred_Data/Fred_Requests.py
+++ BackEnd/Fred_Data/Fred_Requests.py
@@ -1,7 +1,17 @@
 import pandas as pd
+import FRED
 
-def get_gdp():
-    df = pd.read_csv('CSV_Data/GDP.csv')
-    print(df)
+def get_gdp(Update_Request = False):
+    if Update_Request == True:
+        FRED.update_GDP_Data()
+    df = pd.read_csv('GDP.csv')
+    return df
 
-get_gdp()
+def get_reccesion_years(Update_Request = False):
+    if Update_Request == True:
+        FRED.update_Recession_Data()
+    df = pd.read_csv('Recessions.csv')
+    df = df[df['value'] != 0]
+    return df
+
+get_reccesion_years()

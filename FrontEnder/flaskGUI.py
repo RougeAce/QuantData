d0303548424243d1a0fb6b0c66102436323d7cf4
Trey Davidson <treydavidson05@icloud.com>
2023-05-09 15:56:49 -0500

    Major Changes to FrontEnd

--- FrontEnder/flaskGUI.py
+++ FrontEnder/flaskGUI.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import main
 
 app = Flask(__name__)
 
@@ -11,14 +12,24 @@ def index():
 def search():
     if request.method == 'POST':
         query = request.form['query']
-        return redirect(url_for('search_results', query=query))
+        print(f"the query is {query} from POST")
+        return render_template('index.html', company_name=main.ticker_name(main.get_ticker(query.upper())))
     else:
         query = request.args.get('query')
-        return render_template('index.html', result=query)
+        name = main.ticker_name(main.get_ticker(query.upper()))
+        print(f"the company name is {name} from GET")
+        return render_template('index.html', company_name=name)
     
 @app.route('/search_results/<query>')
 def query_results(query):
-    return render_template('index.html', result=query)
+    print(f"the query is {query} from REDIRECT")
+    return render_template('index.html', result=query, company_name=main.ticker_name(main.get_ticker(query.upper())))
+
+# This is so an API or the computer itself can request data
+@app.route('/api/<query>')
+def get_stock_data(query):
+    data = main.current_share_price(main.get_ticker(query.upper()))
+    return str(data)
 
 if __name__ == '__main__':
     app.run()

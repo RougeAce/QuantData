0501472e7fbafd44c7d61f2ca9ef41fe7f70261c
Trey Davidson <treydavidson05@icloud.com>
2023-05-21 22:05:31 -0500

    Added Search_Bar

--- FrontEnder/flaskGUI.py
+++ FrontEnder/flaskGUI.py
@@ -1,11 +1,46 @@
-from flask import Flask, render_template
-import UI
+from flask import Flask, render_template, jsonify, redirect, request, url_for
+import main
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('Stock.html')
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('search_query')
+    print(f"Received query: {query}")
+    return redirect(f"/{query}", code=302)
+
+@app.route('/<query>')
+def search_stock(query):
+    try:
+        ticker = main.get_ticker(symbol=query)
+        stock_price = main.current_share_price(ticker)
+        stock_name = main.ticker_name(ticker)
+        return render_template('Stock.html', Corp_name=stock_name, stock_price=stock_price, ticker=query)
+    except Exception as e:
+        response = f"Could not find place {e}"
+        return jsonify(response)
+
+
+@app.route('/stock_price/<query>')
+def search_stock_price(query):
+    ticker = main.get_ticker(symbol=str(query))
+    stock_price = main.current_share_price(ticker)
+    return jsonify(stock_price=stock_price)
+
+@app.route('/stock_name/<query>')
+def search_stock_name(query):
+    ticker = main.get_ticker(symbol=str(query))
+    stock_name = main.ticker_name(ticker)
+    return jsonify(stock_name=stock_name)
+
+
+
 
 if __name__ == '__main__':
     app.run()
--- FrontEnder/static/script.js
+++ FrontEnder/static/script.js
@@ -1,24 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // get a reference to the stock price element in the HTML
-    const stockPriceElement = document.getElementById('stock-price');
+// Function to handle the search input
+function searchStock(event) {
+    if (event.keyCode === 13) {
+        var input = document.getElementById("searchInput");
+        var query = input.value;
 
-    // define a function to fetch data from the API and update the stock price element
-    function updateStockPrice(query) {
-      fetch(`/api/${query}`)
-        .then(response => response.text())
-        .then(data => {
-          // update the stock price element with the retrieved data
-          stockPriceElement.textContent = data;
-          console.log(data);
-        })
-        .catch(error => console.error(error));
+        // Redirect to the search route for the entered query
+        window.location.href = "/" + query;
     }
+}
 
-    // call the updateStockPrice function initially to set the initial value
-    updateStockPrice('AAPL'); // replace 'AAPL' with your desired query parameter
 
-    // call the updateStockPrice function every second using setInterval()
-    setInterval(() => {
-      updateStockPrice('AAPL'); // replace 'AAPL' with your desired query parameter
-    }, 1000); // repeat every 1000 milliseconds (1 second) 
-  });
+
